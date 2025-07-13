a8267504fd46e6e9a5ce88a82503877246d822a0
kdsoo1 <fahimadls10@gmail.com>
2025-07-13 15:41:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1113120949370359860', // Buyer
-        couleur: '#6495ED', // Couleur HEX
+        couleur: '#c517faff', // Couleur HEX
         footer: '[+] Gl0bal Sh0p', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
