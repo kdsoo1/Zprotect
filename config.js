27288b4a6c8b58da28a816799f10156cd94b8289
kdsoo1 <fahimadls10@gmail.com>
2025-07-13 15:40:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1113120949370359860', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: 'Zprotect', // Dans la commande help ou autre il y'a un texte en bas
+        footer: '[+] Gl0bal Sh0p', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
